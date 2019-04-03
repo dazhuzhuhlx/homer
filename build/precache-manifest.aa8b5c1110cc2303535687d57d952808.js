39d6f8e29e5603c02d6b4a3319296dfd88fc7244
dazhuzhuhlx <2482160283@qq.com>
2019-04-03 16:18:41 +0800

    init

---
+++ build/precache-manifest.aa8b5c1110cc2303535687d57d952808.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "fefc176e146ccc9095de5d70af291ceb",
+    "url": "/static/media/lb.fefc176e.png"
+  },
+  {
+    "revision": "8568f1614832d1949947",
+    "url": "/static/css/main.92801269.chunk.css"
+  },
+  {
+    "revision": "700f6dfb6d58b8d9ccccd10f52369677",
+    "url": "/static/media/分类课程-月饼@3x.700f6dfb.png"
+  },
+  {
+    "revision": "0817c060c1ce7dd6feb9",
+    "url": "/static/js/1.0817c060.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6a441d9bdbbf676667624875ceff2cf5",
+    "url": "/static/media/分类课程-糖@2x.6a441d9b.png"
+  },
+  {
+    "revision": "3461c4f58edc222462a6d36612647939",
+    "url": "/static/media/首页图2.3461c4f5.jpg"
+  },
+  {
+    "revision": "bced0acf0dfc53560b1e3edc6e045418",
+    "url": "/static/media/profileoh.bced0acf.jpg"
+  },
+  {
+    "revision": "52d1a37edb96b4a5e1e83282adf5ebc8",
+    "url": "/static/media/明星课程查看更多-儿童食品@3x.52d1a37e.png"
+  },
+  {
+    "revision": "d12e903c2808470818c2de1daab54f49",
+    "url": "/static/media/明星课程查看更多-缺铁@2x.d12e903c.png"
+  },
+  {
+    "revision": "3f83df08d5bb20f0e5fcd3316fab863d",
+    "url": "/static/media/明星课程查看更多-糖@2x.3f83df08.png"
+  },
+  {
+    "revision": "4514a53512c851c3978172dfb3df0834",
+    "url": "/static/media/最新课程banner@2x.4514a535.png"
+  },
+  {
+    "revision": "8568f1614832d1949947",
+    "url": "/static/js/main.8568f161.chunk.js"
+  },
+  {
+    "revision": "ac8d1993faa5863c370ca59656203a74",
+    "url": "/static/media/首页-明星课程-糖@2x.ac8d1993.png"
+  },
+  {
+    "revision": "316cac26bf0cb1ee558f603bb7020d90",
+    "url": "/static/media/首页-明星课程-补铁@3x.316cac26.png"
+  },
+  {
+    "revision": "b2a0dfcbeffef0fb46e843d7ad76479a",
+    "url": "/static/media/首页-明星课程-儿童食品@2x.b2a0dfcb.png"
+  },
+  {
+    "revision": "d2848e04034cd5cc7f12311631d1f13e",
+    "url": "/static/media/分类课程-咖喱饭@2x.d2848e04.png"
+  },
+  {
+    "revision": "a770dc2e12ceda1cf6f73743aa7d6bc1",
+    "url": "/static/media/分类课程-汤圆@2x.a770dc2e.png"
+  },
+  {
+    "revision": "aaeb39cd0d3ce0b6fac7d5e5a0725ac4",
+    "url": "/static/media/最新课程banner@3x.aaeb39cd.png"
+  },
+  {
+    "revision": "1c9705e06ae317d8abba39e37a75c5cf",
+    "url": "/static/media/首页图3.1c9705e0.jpg"
+  },
+  {
+    "revision": "ee25193c3dd30be6c66074118438a48b",
+    "url": "/static/media/分类课程-咖喱饭@3x.ee25193c.png"
+  },
+  {
+    "revision": "f2710e9cdbd23b1fac64091e76ce1520",
+    "url": "/static/media/分类课程-妈咪必备@2x.f2710e9c.png"
+  },
+  {
+    "revision": "0817c060c1ce7dd6feb9",
+    "url": "/static/css/1.5f6c76d1.chunk.css"
+  },
+  {
+    "revision": "1d77d4c64c58d44f15cc0737f45e6d52",
+    "url": "/index.html"
+  }
+];
